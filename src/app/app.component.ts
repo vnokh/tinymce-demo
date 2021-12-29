ae3e550a3d163c46d6058bc69d61310b42a0e471
Vikas Nokhwal <vnokh@live.com>
2021-12-29 10:30:14 +0800

    tinymce integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tinymce-demo';
+
+  tinyMCEConfig = {
+    base_url: '/tinymce',
+    suffix: '.min',
+    height: 500,
+    menubar: true,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste code help wordcount'
+    ],
+    toolbar:
+      'undo redo | formatselect | bold italic backcolor | \
+       alignleft aligncenter alignright alignjustify | \
+       bullist numlist outdent indent | removeformat | help'
+  };
 }
